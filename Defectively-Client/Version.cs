3d49351110b5721ceda5d40d6d6a3baf22e7b536
vainamov <vainamov@users.noreply.github.com>
2017-02-24 17:54:06 +0100

    Version Reset

--- Defectively-Client/Version.cs
+++ Defectively-Client/Version.cs
@@ -4,12 +4,12 @@ namespace DefectivelyClient
 {
     public class Version : Defectively.Compatibility.Version
     {
-        public override int Major { get; set; } = 2;
-        public override int Minor { get; set; } = 2;
-        public override int Patch { get; set; } = 42;
+        public override int Major { get; set; } = 0;
+        public override int Minor { get; set; } = 0;
+        public override int Patch { get; set; } = 1;
         public override VersioningProfiler.Suffixes Suffix { get; set; } = VersioningProfiler.Suffixes.none;
-        public override string ReleaseDate { get; set; } = "17w05";
-        public override string Commit { get; set; } = "5f510e5"; // #festival-version-control new
-        public override string SupportedVersion { get; set; } = "2.2.0-alpha [b236067]";
+        public override string ReleaseDate { get; set; } = "17w08";
+        public override string Commit { get; set; } = "68db94d";
+        public override string SupportedVersion { get; set; } = "0.0.1 [48d4440]";
     }
 }
